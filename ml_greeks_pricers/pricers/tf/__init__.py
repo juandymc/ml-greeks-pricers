e8d51033974808c6fb0f450623e9a687a64a0cfa
juandymc <65504525+juandymc@users.noreply.github.com>
2025-06-29 21:32:17 +0200

    Use explicit imports in tf pricers

--- ml_greeks_pricers/pricers/tf/__init__.py
+++ ml_greeks_pricers/pricers/tf/__init__.py
@@ -1,7 +1,9 @@
-from .american import *
-from .european import *
-from .basket import *
-from .black_utils import *
+# Explicitly import symbols instead of relying on star imports to
+# avoid missing attributes when the package is partially installed.
+from .american import MarketData, AmericanAsset, MCAmericanOption
+from .european import EuropeanAsset, MCEuropeanOption
+from .basket import BasketAsset, MCWorstOfOption
+from .black_utils import bs_price
 
 __all__ = [
     "MarketData",
